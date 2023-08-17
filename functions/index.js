338b0385be1b34c3935167f8f75dc3dae116cbbe
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 23:23:08 +0800

    Adding pinecone

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-const {log} = require( "firebase-functions/logger");
+const {log, error} = require( "firebase-functions/logger");
 const {onObjectFinalized} = require( "firebase-functions/v2/storage");
 const {extractPDF} = require( "./textExtractor/extractPDF");
 const {getStorage} = require( "firebase-admin/storage");
 const {initializeApp} = require( "firebase-admin/app");
 const {setGlobalOptions} = require("firebase-functions/v2");
 const {getFirestore} = require("firebase-admin/firestore");
-const {onDocumentDeleted} = require("firebase-functions/v2/firestore");
+const {onDocumentDeleted, onDocumentWritten} = require("firebase-functions/v2/firestore");
 
 // Init firebase function app
 initializeApp();
@@ -26,8 +26,6 @@ exports.convertFileToText = onObjectFinalized(async (event) => {
 
   const fileBucket = event.data.bucket; // Storage bucket containing the file.
 
-  log("userid", userid);
-  log("collectionid", collectionid);
   const contentType = event.data.contentType; // File content type.
   const acceptedType = new Set(["application/pdf"]);
   // Exit if this is triggered on a file that is not an image.
@@ -39,7 +37,6 @@ exports.convertFileToText = onObjectFinalized(async (event) => {
   const bucket = getStorage().bucket(fileBucket);
   const downloadResponse = await bucket.file(filePath).download();
   const fileBuffer = downloadResponse[0];
-  log("File downloaded!");
 
   let text;
   if (contentType == "application/pdf") {
@@ -56,6 +53,7 @@ exports.convertFileToText = onObjectFinalized(async (event) => {
 const bucketName = "gs://better-notes-b6af7.appspot.com";
 const firestoreDocumentPath =
   "users/{userid}/collections/{collectionid}/documents/{documentid}";
+
 exports.appDocumentCleanUp =
   onDocumentDeleted(firestoreDocumentPath, (event) => {
     const {userid, collectionid, documentid} = event.params;
@@ -63,6 +61,49 @@ exports.appDocumentCleanUp =
         .file(`${userid}/documents/${collectionid}/${documentid}`)
         .delete();
   });
+
+
+const {PineconeClient} = require("@pinecone-database/pinecone");
+const {uploadTextToPinecone} = require("./pinecone/uploadTextToPinecone");
+// setup pinecone
+const PINECONE_ENVIRONMENT = "gcp-starter";
+const PINECONE_INDEX = "betternotes";
+
+exports.generateVectors =
+  onDocumentWritten({secrets: ["PINECONE_API_KEY", "OPENAI_API_KEY"]},
+      firestoreDocumentPath, async (event) => {
+        if ( !event.data.after.data() ||
+      !("content" in event.data.after.data()) ||
+        event.data.before.data().content ==
+        event.data.after.data().content) return;
+        log("past check");
+        log(process.env.PINECONE_API_KEY);
+        try {
+          const documentId = event.data.after.data().id;
+          const content = event.data.after.data().content;
+          const source = event.data.after.data().downloadURL;
+          const {userid, collectionid, documentid} = event.params;
+          const client = new PineconeClient();
+          await client.init({
+            apiKey: process.env.PINECONE_API_KEY,
+            environment: PINECONE_ENVIRONMENT,
+          });
+          const vectorIds = await uploadTextToPinecone(
+              client, PINECONE_INDEX, collectionid, documentId, content, source);
+
+          // update document
+          db.collection("users").doc(userid)
+              .collection("collections").doc(collectionid)
+              .collection("documents").doc(documentid)
+              .update({
+                vectorIds: vectorIds,
+                status: "ready",
+              });
+          log("Done");
+        } catch (err) {
+          error(err);
+        }
+      });
 
 // exports.saveOCRResult = onObjectFinalized(async (event) => {
 //   const filePath = event.data.name; // File path in the bucket.
---
+++ functions/pinecone/uploadTextToPinecone.js
@@ -0,0 +1,70 @@
+// 1. Import required modules
+const {log} = require("firebase-functions/logger");
+const {OpenAIEmbeddings} = require("langchain/embeddings/openai");
+const {RecursiveCharacterTextSplitter} = require("langchain/text_splitter");
+// 2. Export updatePinecone function
+module.exports.uploadTextToPinecone =
+  async function(client, indexName, namespace, documentId, text, source) {
+    log("Retrieving Pinecone index...");
+    // 3. Retrieve Pinecone index
+    // eslint-disable-next-line new-cap
+    const index = client.Index(indexName);
+    // 4. Log the retrieved index name
+    log(`Pinecone index retrieved: ${indexName}`);
+    // 5. Process text
+    log(`Processing document`);
+    // 6. Create RecursiveCharacterTextSplitter instance
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+    });
+    log("Splitting text into chunks...");
+    // 7. Split text into chunks (documents)
+    const chunks = await textSplitter.createDocuments([text]);
+    log(`Text split into ${chunks.length} chunks`);
+    log(
+        `Calling Embedding endpoint with ${chunks.length} text chunks ...`,
+    );
+    // 8. Create OpenAI embeddings for documents
+    const embeddingsArrays = await new OpenAIEmbeddings({openAiApiKey: process.env.OPENAI_API_KEY}).embedDocuments(
+        chunks.map((chunk) => chunk.pageContent.replace(/\n/g, " ")),
+    );
+    log("Finished embedding documents");
+    log(
+        `Creating ${chunks.length} vectors array ...`,
+    );
+    // 9. Create and upsert vectors in batches of 100
+    const batchSize = 100;
+    const vectorIds = [];
+    let batch = [];
+    for (let idx = 0; idx < chunks.length; idx++) {
+      const chunk = chunks[idx];
+      const vectorId = `${documentId}_${idx}`;
+      const vector = {
+        id: vectorId,
+        values: embeddingsArrays[idx],
+        metadata: {
+          ...chunk.metadata,
+          loc: JSON.stringify(chunk.metadata.loc),
+          pageContent: chunk.pageContent,
+          txtPath: source,
+        },
+        namespace: documentId,
+      };
+      batch.push(vector);
+      vectorIds.push(vectorId);
+
+      // When batch is full or it's the last item, upsert the vectors
+      if (batch.length === batchSize || idx === chunks.length - 1) {
+        await index.upsert({
+          upsertRequest: {
+            vectors: batch,
+          },
+        });
+        // Empty the batch
+        batch = [];
+      }
+    }
+    // 10. Log the number of vectors updated
+    log(`Pinecone index updated with ${chunks.length} vectors`);
+    return vectorIds;
+  };
