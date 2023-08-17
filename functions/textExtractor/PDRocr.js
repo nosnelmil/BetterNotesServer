dfc454e87d02a72f25b3778371e6b77671f325fb
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 22:02:53 +0800

    Create PDRocr.js

---
+++ functions/textExtractor/PDRocr.js
@@ -0,0 +1,36 @@
+// Imports the Google Cloud client libraries
+const vision = require("@google-cloud/vision").v1;
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+module.exports.extractPDF = async function(
+    fileBucket, filePath, userid, collectionid, documentid) {
+  const gcsSourceUri = "gs://" + fileBucket + "/" + filePath;
+  const inputConfig = {
+    // Supported mime_types are: 'application/pdf' and 'image/tiff'
+    mimeType: "application/pdf",
+    gcsSource: {
+      uri: gcsSourceUri,
+    },
+  };
+  const outputConfig = {
+    gcsDestination: {
+      uri: `gs://${fileBucket}/${userid}/output/${collectionid}/${documentid}`,
+    },
+  };
+
+  const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
+  const request = {
+    requests: [
+      {
+        inputConfig: inputConfig,
+        features: features,
+        outputConfig: outputConfig,
+      },
+    ],
+  };
+
+  client.asyncBatchAnnotateFiles(request);
+};
+
