cfeede25acdbedd104d1210ec03dab60487e3104
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-17 23:37:50 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,17 @@ const {getStorage} = require( "firebase-admin/storage");
 const {initializeApp} = require( "firebase-admin/app");
 const {setGlobalOptions} = require("firebase-functions/v2");
 const {getFirestore} = require("firebase-admin/firestore");
-const {onDocumentDeleted, onDocumentWritten} = require("firebase-functions/v2/firestore");
+const {onDocumentDeleted, onDocumentWritten} =
+  require("firebase-functions/v2/firestore");
 
 // Init firebase function app
 initializeApp();
 
 
-setGlobalOptions({region: "asia-southeast2", cpu: "gcf_gen1"});
+setGlobalOptions({
+  region: "asia-southeast2",
+  cpu: "gcf_gen1",
+  secrets: ["PINECONE_API_KEY", "OPENAI_API_KEY"]});
 
 const db = getFirestore();
 const storage = getStorage();
@@ -70,40 +74,39 @@ const PINECONE_ENVIRONMENT = "gcp-starter";
 const PINECONE_INDEX = "betternotes";
 
 exports.generateVectors =
-  onDocumentWritten({secrets: ["PINECONE_API_KEY", "OPENAI_API_KEY"]},
-      firestoreDocumentPath, async (event) => {
-        if ( !event.data.after.data() ||
+  onDocumentWritten(firestoreDocumentPath, async (event) => {
+    if ( !event.data.after.data() ||
       !("content" in event.data.after.data()) ||
         event.data.before.data().content ==
         event.data.after.data().content) return;
-        log("past check");
-        log(process.env.PINECONE_API_KEY);
-        try {
-          const documentId = event.data.after.data().id;
-          const content = event.data.after.data().content;
-          const source = event.data.after.data().downloadURL;
-          const {userid, collectionid, documentid} = event.params;
-          const client = new PineconeClient();
-          await client.init({
-            apiKey: process.env.PINECONE_API_KEY,
-            environment: PINECONE_ENVIRONMENT,
-          });
-          const vectorIds = await uploadTextToPinecone(
-              client, PINECONE_INDEX, collectionid, documentId, content, source);
-
-          // update document
-          db.collection("users").doc(userid)
-              .collection("collections").doc(collectionid)
-              .collection("documents").doc(documentid)
-              .update({
-                vectorIds: vectorIds,
-                status: "ready",
-              });
-          log("Done");
-        } catch (err) {
-          error(err);
-        }
+    log("past check");
+    log(process.env.PINECONE_API_KEY);
+    try {
+      const documentId = event.data.after.data().id;
+      const content = event.data.after.data().content;
+      const source = event.data.after.data().downloadURL;
+      const {userid, collectionid, documentid} = event.params;
+      const client = new PineconeClient();
+      await client.init({
+        apiKey: process.env.PINECONE_API_KEY,
+        environment: PINECONE_ENVIRONMENT,
       });
+      const vectorIds = await uploadTextToPinecone(
+          client, PINECONE_INDEX, collectionid, documentId, content, source);
+
+      // update document
+      db.collection("users").doc(userid)
+          .collection("collections").doc(collectionid)
+          .collection("documents").doc(documentid)
+          .update({
+            vectorIds: vectorIds,
+            status: "ready",
+          });
+      log("Done");
+    } catch (err) {
+      error(err);
+    }
+  });
 
 // exports.saveOCRResult = onObjectFinalized(async (event) => {
 //   const filePath = event.data.name; // File path in the bucket.
