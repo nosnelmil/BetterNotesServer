3db07554b889fd539fbd442e28a2efcad9191b96
Lenson <53988015+Lebarnon@users.noreply.github.com>
2023-08-23 17:40:16 +0800

    Added chunking deleteVectors

--- functions/pinecone/deleteIndexes.js
+++ functions/pinecone/deleteIndexes.js
@@ -10,5 +10,10 @@ module.exports.deleteIndexes =
     // 4. Log the retrieved index name
     log(`Pinecone index retrieved: ${indexName}`);
     // 5. Process text
-    await index.delete1({ids: vectorIds});
+    const chunkSize = 90;
+    for (let i = 0; i < Math.ceil(vectorIds.length / chunkSize); i++) {
+      await index.delete1({
+        ids: vectorIds.slice(i*chunkSize, (i+1)*chunkSize),
+      });
+    }
   };
